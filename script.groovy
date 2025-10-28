f533d93e781f5d77ac22e8879b4559beaa7a6ced
rakhel9 <151727963+rakhel9@users.noreply.github.com>
2025-10-28 13:37:22 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,9 @@
+def test() {
+    echo 'Testing the app..'
+    echo "Executing Pipeline for $BRANCH_NAME"
+    sh 'mvn test'
+}
+
 def buildJar() {
     echo 'Building the app..'
     sh 'mvn package'
@@ -6,9 +12,9 @@ def buildJar() {
 def buildImage() {
     echo 'Building the app..' 
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential',passwordVariable: 'PASS',usernameVariable: 'USER')]) {
-        sh 'docker build -t rakhel/jenkins-build:jma-4.0 .'
+        sh 'docker build -t rakhel/jenkins-build:jma-3.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push rakhel/jenkins-build:jma-4.0'    
+        sh 'docker push rakhel/jenkins-build:jma-2.0'    
     }
 }
 
